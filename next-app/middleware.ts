6ecf8721ad17ff3a4752e7ba0a3654ce9c9e9a15
alighieth <alighieth2709@gmail.com>
2023-08-31 20:08:57 +0300

    Update middleware.ts

--- next-app/middleware.ts
+++ next-app/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest) {
   if (!isLoggedInFlag && !String(request.url).includes("/login"))
     return NextResponse.redirect(new URL("/login", request.url));
   if (isLoggedInFlag && String(request.url).includes("/login"))
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
 
   return NextResponse.next();
 }
